86adc5b281c0ce411a2d62eaf64fb62789bce374
voidwu <wutianyitower@hotmail.com>
2023-11-28 15:29:33 +0800

    rename as dl_utils

--- utils/dl_utils.py
+++ utils/dl_utils.py
@@ -1,21 +1,27 @@
 #!/usr/bin python3
 # -*- coding: utf-8 -*-
 """
-@Time    :   2023/06/07 16:52:41
+@Time    :   2023/11/28 15:00:10
 @Author  :   Tianyi Wu 
 @Contact :   wutianyitower@hotmail.com
-@File    :   base_utils.py
+@File    :   dl_utils.py
 @Version :   1.0
 @Desc    :   None
 """
 
+
 import logging
 import os
 import random
+import sys
 import time
 
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from matplotlib_inline import backend_inline
+
+dl_utils = sys.modules[__name__]
 
 
 # NOTE: 为了保证实验的可重复性，需要设置随机数种子。
@@ -188,5 +194,71 @@ class EarlyStopping:
         self.val_loss_min = val_loss
 
 
+def use_svg_display():
+    """Use the svg format to display a plot in Jupyter."""
+    backend_inline.set_matplotlib_formats("svg")
+
+
+def set_figsize(figsize=(3.5, 2.5)):
+    """Set the figure size for matplotlib."""
+    use_svg_display()
+    dl_utils.plt.rcParams["figure.figsize"] = figsize
+
+
+def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+    """设置matplotlib的轴"""
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale)
+    axes.set_yscale(yscale)
+    axes.set_xlim(xlim)
+    axes.set_ylim(ylim)
+    if legend:
+        axes.legend(legend)
+    axes.grid()
+
+
+def plot(
+    X,
+    Y=None,
+    xlabel=None,
+    ylabel=None,
+    legend=None,
+    xlim=None,
+    ylim=None,
+    xscale="linear",
+    yscale="linear",
+    fmts=("-", "m--", "g-.", "r:"),
+    figsize=(3.5, 2.5),
+    axes=None,
+):
+    """绘制数据点"""
+    if legend is None:
+        legend = []
+
+    set_figsize(figsize)
+    axes = axes if axes else dl_utils.plt.gca()
+
+    def has_one_axis(X):
+        """如果X有一个轴，输出True"""
+        return hasattr(X, "ndim") and X.ndim == 1 or isinstance(X, list) and not hasattr(X[0], "__len__")
+
+    if has_one_axis(X):
+        X = [X]
+    if Y is None:
+        X, Y = [[]] * len(X), X
+    elif has_one_axis(Y):
+        Y = [Y]
+    if len(X) != len(Y):
+        X = X * len(Y)
+    axes.cla()
+    for x, y, fmt in zip(X, Y, fmts):
+        if len(x):
+            axes.plot(x, y, fmt)
+        else:
+            axes.plot(y, fmt)
+    set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+
+
 if __name__ == "__main__":
-    setup_config()
+    print(sys.modules)
