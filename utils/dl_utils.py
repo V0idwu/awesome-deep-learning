c8ffb2981b65ed874efd293428d234fbd3f714a5
voidwu <wutianyitower@hotmail.com>
2023-12-01 15:27:36 +0800

    add Timer

--- utils/dl_utils.py
+++ utils/dl_utils.py
@@ -260,5 +260,45 @@ def plot(
     set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
 
 
+class Timer:  # @save
+    """记录多次运行时间"""
+
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        """启动计时器"""
+        self.tik = time.time()
+
+    def stop(self):
+        """停止计时器并将时间记录在列表中"""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """返回平均时间"""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """返回时间总和"""
+        return sum(self.times)
+
+    def cumsum(self):
+        """返回累计时间"""
+        return np.array(self.times).cumsum().tolist()
+
+
 if __name__ == "__main__":
-    print(sys.modules)
+    n = 10000
+    a = torch.ones([n])
+    b = torch.ones([n])
+    c = torch.zeros(n)
+    timer = Timer()
+    for i in range(n):
+        c[i] = a[i] + b[i]
+    print(f"{timer.stop():.5f} sec")
+
+    timer.start()
+    d = a + b
+    print(f"{timer.stop():.5f} sec")
