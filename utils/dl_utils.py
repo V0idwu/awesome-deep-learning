9bdbafdbffb8e06a6f87b644f89bd7312be8701c
voidwu <wutianyitower@hotmail.com>
2023-12-05 10:15:26 +0800

    update dl_utils

--- utils/dl_utils.py
+++ utils/dl_utils.py
@@ -19,6 +19,7 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from IPython import display
 from matplotlib_inline import backend_inline
 
 dl_utils = sys.modules[__name__]
@@ -55,6 +56,11 @@ def setup_config(seed: int = 227):
 # dataloader = DataLoader(dataset, batch_size=4, shuffle=True, worker_init_fn=worker_init_fn)
 def worker_init_fn(worker_id, global_seed=1):
     setup_config(global_seed + worker_id)
+
+
+def get_dataloader_workers():
+    """使用4个进程来读取数据"""
+    return 4
 
 
 def try_gpu(i: int = 0):
@@ -289,16 +295,174 @@ class Timer:  # @save
         return np.array(self.times).cumsum().tolist()
 
 
-if __name__ == "__main__":
-    n = 10000
-    a = torch.ones([n])
-    b = torch.ones([n])
-    c = torch.zeros(n)
-    timer = Timer()
-    for i in range(n):
-        c[i] = a[i] + b[i]
-    print(f"{timer.stop():.5f} sec")
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):  # @save
+    """绘制图像列表"""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = dl_utils.plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        if torch.is_tensor(img):
+            # 图片张量
+            ax.imshow(img.numpy())
+        else:
+            # PIL图片
+            ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
 
-    timer.start()
-    d = a + b
-    print(f"{timer.stop():.5f} sec")
+
+def get_dataloader_workers():  # @save
+    """使用4个进程来读取数据"""
+    return 4
+
+
+def accuracy(y_hat, y):  # @save
+    """计算预测正确的数量"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+
+class Accumulator:  # @save
+    """在n个变量上累加"""
+
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+# 同样，对于任意数据迭代器`data_iter`可访问的数据集，[**我们可以评估在任意模型`net`的精度**]。
+def evaluate_accuracy(net, data_iter):  # @save
+    """计算在指定数据集上模型的精度"""
+    if isinstance(net, torch.nn.Module):
+        net.eval()  # 将模型设置为评估模式
+    metric = Accumulator(2)  # 正确预测数、预测总数
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def train_epoch(net, train_iter, loss, updater):
+    """训练模型一个迭代周期"""
+    # 将模型设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            # 使用定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练精度
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+class Animator:  # @save
+    """在动画中绘制数据"""
+
+    def __init__(
+        self,
+        xlabel=None,
+        ylabel=None,
+        legend=None,
+        xlim=None,
+        ylim=None,
+        xscale="linear",
+        yscale="linear",
+        fmts=("-", "m--", "g-.", "r:"),
+        nrows=1,
+        ncols=1,
+        figsize=(3.5, 2.5),
+    ):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        dl_utils.use_svg_display()
+        self.fig, self.axes = dl_utils.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [
+                self.axes,
+            ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: dl_utils.set_axes(self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+
+def train_epoch(net, train_iter, loss, updater):  # @save
+    """训练模型一个迭代周期"""
+    # 将模型设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            # 使用定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练精度
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+def train(net, train_iter, test_iter, loss, num_epochs, updater):  # @save
+    """训练模型"""
+    animator = Animator(xlabel="epoch", xlim=[1, num_epochs], ylim=[0.3, 0.9], legend=["train loss", "train acc", "test acc"])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
